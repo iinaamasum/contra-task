bfa9f7ce6e96e9a40a0b51d288fb78e864c2a44c
iinaamasum <iinaamasum@gmail.com>
2022-08-02 16:34:47 +0600

    input box beutification done

--- src/Components/Auth/Login.jsx
+++ src/Components/Auth/Login.jsx
@@ -5,11 +5,23 @@ const Login = () => {
     <section>
       <article className="flex items-center justify-center h-[80vh]">
         <div className="bg-[#fefefe] rounded-xl shadow-2xl p-5 w-full md:w-1/2 mx-auto text-center">
-          <h2 className="text-2xl md:text-4xl font-bold">
+          <h2 className="text-2xl md:text-4xl font-bold my-5">
             Welcome back to Contra
           </h2>
           <form>
-            <input className="bg-gray-300" type="text" />
+            <div className="border-2 rounded-t-lg w-full md:w-[70%] mx-auto flex flex-col text-left">
+              <label
+                className="text-gray-500 text-sm mt-1 px-4 font-medium tracking-wide"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <input
+                className="py-1 px-4 focus:outline-none text-sm border-b-2 focus:border-blue-600"
+                type="text"
+              />
+            </div>
+            <br />
             <input className="bg-gray-300" type="submit" value="Log In" />
           </form>
         </div>
