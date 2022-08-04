7e9f9b79e68b6d232c2d2577eb8a7bd54637e673
iinaamasum <iinaamasum@gmail.com>
2022-08-04 18:59:25 +0600

    pin-enter-value switch input box done

--- src/Components/Auth/PinVerifyInputBox.jsx
+++ src/Components/Auth/PinVerifyInputBox.jsx
@@ -1,44 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const PinVerifyInputBox = () => {
+  const [enteredPin, setEnteredPin] = useState({
+    val1: '',
+    val2: '',
+    val3: '',
+    val4: '',
+  });
+  const handleChange = (e) => {
+    const { maxLength, value, name } = e.target;
+    const [fieldName, fieldIndex] = name.split('-');
+
+    console.log(fieldName);
+
+    if (value.length >= maxLength) {
+      if (parseInt(fieldIndex) < 4) {
+        const nextSibling = document.querySelector(
+          `input[name=val-${parseInt(fieldIndex) + 1}]`
+        );
+
+        if (nextSibling !== null) {
+          nextSibling.focus();
+        }
+      }
+      setEnteredPin({
+        ...enteredPin,
+        [`val${fieldIndex}`]: value,
+      });
+    }
+  };
   return (
-    <div class="w-full flex items-center justify-center mt-5">
-      <div class="border-2 border-orange-500 rounded-2xl px-5 py-3 grid grid-cols-6 gap-6">
+    <div className="w-full flex items-center justify-center mt-5">
+      <div className="border-2 border-[#F9E4A6] rounded-2xl py-3 grid grid-cols-4 gap-6 px-10">
         <input
           maxLength={1}
-          className="w-5 text-2xl font-bold text-orange-500 focus:outline-none"
+          className="w-5 text-2xl font-bold text-orange-500 focus:outline-none placeholder:text-5xl placeholder:text-gray-400"
           type="text"
+          name="val-1"
           placeholder="."
+          onChange={handleChange}
         />
         <input
           maxLength={1}
-          className="w-5 text-2xl font-bold text-orange-500 focus:outline-none"
+          className="w-5 text-2xl font-bold text-orange-500 focus:outline-none placeholder:text-5xl placeholder:text-gray-400"
           type="text"
+          name="val-2"
           placeholder="."
+          onChange={handleChange}
         />
         <input
           maxLength={1}
-          className="w-5 text-2xl font-bold text-orange-500 focus:outline-none placeholder:text-3xl"
+          className="w-5 text-2xl font-bold text-orange-500 focus:outline-none placeholder:text-5xl placeholder:text-gray-400"
           type="text"
+          name="val-3"
           placeholder="."
+          onChange={handleChange}
         />
         <input
           maxLength={1}
-          className="w-5 text-2xl font-bold text-orange-500 focus:outline-none"
+          className="w-5 text-2xl font-bold text-orange-500 focus:outline-none placeholder:text-5xl placeholder:text-gray-400"
           type="text"
           placeholder="."
-        />
-        <input
-          maxLength={1}
-          className="w-5 text-2xl font-bold text-orange-500 focus:outline-none"
-          type="text"
-          placeholder="."
-        />
-        <input
-          maxLength={1}
-          className="w-5 text-2xl font-bold text-orange-500 focus:outline-none"
-          type="text"
-          placeholder="."
+          name="val-4"
+          onChange={handleChange}
         />
       </div>
     </div>
--- src/Components/Shared/Divider.jsx
+++ src/Components/Shared/Divider.jsx
@@ -5,7 +5,7 @@ const Divider = () => {
     <section className="text-center my-4">
       <div className="flex items-center justify-center">
         <div className="w-28 border-b-2 border-gray-300"></div>
-        <p class="text-gray-500 text-xs px-2">OR</p>
+        <p className="text-gray-500 text-xs px-2">OR</p>
         <div className="w-28 border-b-2 border-gray-300"></div>
       </div>
     </section>
