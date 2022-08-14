dcb770e1eaa1295174b49bea52317726f5619e2b
iinaamasum <iinaamasum@gmail.com>
2022-08-15 01:57:38 +0600

    suggession back

--- src/Components/SocialLinks/AddedLinksTable.jsx
+++ src/Components/SocialLinks/AddedLinksTable.jsx
@@ -4,10 +4,26 @@ import { HiOutlineExternalLink } from 'react-icons/hi';
 import { TbGridDots, TbTrash } from 'react-icons/tb';
 import { VscEdit } from 'react-icons/vsc';
 
-const AddedLinksTable = ({ addedLinks, setAddedLinks }) => {
-  const handleDeleteTableData = (link) => {
-    const remaining = addedLinks.filter((filterLink) => filterLink !== link);
-    setAddedLinks([remaining]);
+const AddedLinksTable = ({
+  addedLinks,
+  setAddedLinks,
+  addedDefaultLinks,
+  setAddedDefaultLinks,
+}) => {
+  const handleDeleteTableData = (link_url, link_name) => {
+    if (link_name.includes('GitHub')) {
+      setAddedDefaultLinks({ ...addedDefaultLinks, github: false });
+    }
+    if (link_name.includes('Instagram')) {
+      setAddedDefaultLinks({ ...addedDefaultLinks, instagram: false });
+    }
+    if (link_name.includes('LinkedIn')) {
+      setAddedDefaultLinks({ ...addedDefaultLinks, linkedin: false });
+    }
+    const remaining = addedLinks.filter(
+      (filterLink) => filterLink.link_url !== link_url
+    );
+    setAddedLinks(remaining);
   };
   console.log(addedLinks);
   return (
@@ -18,70 +34,66 @@ const AddedLinksTable = ({ addedLinks, setAddedLinks }) => {
             <table class="min-w-full divide-y divide-gray-200">
               <tbody class="">
                 <tr>
-                  {addedLinks.map((link) =>
-                    link === '' ? (
-                      ''
-                    ) : (
-                      <div
-                        className="cursor-pointer hover:bg-[#eeeeee] flex items-center justify-between rounded-lg"
-                        key={link.link_url}
-                      >
-                        <td className="w-[80%]">
-                          <a
-                            title={link.link_url}
-                            href={link.link_url}
-                            target="_blank"
-                            rel="noreferrer"
-                            className="w-full"
-                          >
-                            <div className="flex items-center gap-x-8 justify-start px-1 py-4">
-                              <TbGridDots size={25} />
-                              <div className="flex items-center justify-start gap-x-7">
-                                {link.link_name
+                  {addedLinks.map((link) => (
+                    <div
+                      className="cursor-pointer hover:bg-[#eeeeee] flex items-center justify-between rounded-lg"
+                      key={link.link_url}
+                    >
+                      <td className="w-[80%]">
+                        <a
+                          title={link.link_url || ''}
+                          href={link.link_url || ''}
+                          target="_blank"
+                          rel="noreferrer"
+                          className="w-full"
+                        >
+                          <div className="flex items-center gap-x-8 justify-start px-1 py-4">
+                            <TbGridDots size={25} />
+                            <div className="flex items-center justify-start gap-x-7">
+                              {link.link_name
+                                ?.toLowerCase()
+                                .includes('github') && <FaGithub size={20} />}
+                              {link.link_name
+                                ?.toLowerCase()
+                                .includes('linkedin') && (
+                                <FaLinkedin size={20} />
+                              )}
+                              {link.link_name
+                                ?.toLowerCase()
+                                .includes('instagram') && (
+                                <FaInstagram size={20} />
+                              )}
+                              {!(
+                                link.link_name
                                   ?.toLowerCase()
-                                  .includes('github') && <FaGithub size={20} />}
-                                {link.link_name
+                                  .includes('github') ||
+                                link.link_name
                                   ?.toLowerCase()
-                                  .includes('linkedin') && (
-                                  <FaLinkedin size={20} />
-                                )}
-                                {link.link_name
+                                  .includes('instagram') ||
+                                link.link_name
                                   ?.toLowerCase()
-                                  .includes('instagram') && (
-                                  <FaInstagram size={20} />
-                                )}
-                                {!(
-                                  link.link_name
-                                    ?.toLowerCase()
-                                    .includes('github') ||
-                                  link.link_name
-                                    ?.toLowerCase()
-                                    .includes('instagram') ||
-                                  link.link_name
-                                    ?.toLowerCase()
-                                    .includes('linkedin')
-                                ) && <HiOutlineExternalLink size={20} />}
-                                <p className="font-semibold">
-                                  {link.link_name}
-                                </p>
-                              </div>
+                                  .includes('linkedin')
+                              ) && <HiOutlineExternalLink size={20} />}
+                              <p className="font-semibold">{link.link_name}</p>
                             </div>
-                          </a>
-                        </td>
-                        <td className="flex items-center gap-x-3 justify-end w-[20%] px-1 py-4">
-                          <VscEdit
-                            size={27}
-                            className="cursor-pointer hover:text-orange-500 font-light"
-                          />
-                          <TbTrash
-                            onClick={() => handleDeleteTableData(link)}
-                            size={27}
-                            className="cursor-pointer hover:text-orange-500 font-light"
-                          />
-                        </td>
-                      </div>
-                    )
-                  )}
+                          </div>
+                        </a>
+                      </td>
+                      <td className="flex items-center gap-x-3 justify-end w-[20%] px-1 py-4">
+                        <VscEdit
+                          size={27}
+                          className="cursor-pointer hover:text-orange-500 font-light"
+                        />
+                        <TbTrash
+                          onClick={() =>
+                            handleDeleteTableData(link.link_url, link.link_name)
+                          }
+                          size={27}
+                          className="cursor-pointer hover:text-orange-500 font-light"
+                        />
+                      </td>
+                    </div>
+                  ))}
                 </tr>
               </tbody>
             </table>
--- src/Components/SocialLinks/SocialLinks.jsx
+++ src/Components/SocialLinks/SocialLinks.jsx
@@ -10,11 +10,9 @@ import {
   FaTwitter,
   FaXbox,
 } from 'react-icons/fa';
-import { HiOutlineExternalLink } from 'react-icons/hi';
-import { TbGridDots, TbTrash } from 'react-icons/tb';
-import { VscEdit } from 'react-icons/vsc';
 import { useNavigate } from 'react-router-dom';
 import Navbar from '../Shared/Navbar';
+import AddedLinksTable from './AddedLinksTable';
 import LinkInputForm from './LinkInputForm';
 
 const SocialLinks = () => {
@@ -54,13 +52,7 @@ const SocialLinks = () => {
       });
     }
   }, [addedLinks]);
-  const handleDeleteTableData = (url) => {
-    console.log('hi', url);
-    const remaining = addedLinks.filter(
-      (filterLink) => filterLink.link_url !== url
-    );
-    setAddedLinks(remaining);
-  };
+
   console.log(addedLinks);
   return (
     <>
@@ -121,88 +113,12 @@ const SocialLinks = () => {
 
                   {/* added links table  */}
                   <div className="mb-[30px]">
-                    <div class="flex flex-col max-w-[425px] mx-auto text-center">
-                      <div class="">
-                        <div class="min-w-full inline-block align-middle">
-                          <div class="overflow-hidden">
-                            <table class="min-w-full divide-y divide-gray-200">
-                              <tbody class="">
-                                <tr>
-                                  {addedLinks.map((link) => (
-                                    <div
-                                      className="cursor-pointer hover:bg-[#eeeeee] flex items-center justify-between rounded-lg"
-                                      key={link.link_url}
-                                    >
-                                      <td className="w-[80%]">
-                                        <a
-                                          title={link.link_url || ''}
-                                          href={link.link_url || ''}
-                                          target="_blank"
-                                          rel="noreferrer"
-                                          className="w-full"
-                                        >
-                                          <div className="flex items-center gap-x-8 justify-start px-1 py-4">
-                                            <TbGridDots size={25} />
-                                            <div className="flex items-center justify-start gap-x-7">
-                                              {link.link_name
-                                                ?.toLowerCase()
-                                                .includes('github') && (
-                                                <FaGithub size={20} />
-                                              )}
-                                              {link.link_name
-                                                ?.toLowerCase()
-                                                .includes('linkedin') && (
-                                                <FaLinkedin size={20} />
-                                              )}
-                                              {link.link_name
-                                                ?.toLowerCase()
-                                                .includes('instagram') && (
-                                                <FaInstagram size={20} />
-                                              )}
-                                              {!(
-                                                link.link_name
-                                                  ?.toLowerCase()
-                                                  .includes('github') ||
-                                                link.link_name
-                                                  ?.toLowerCase()
-                                                  .includes('instagram') ||
-                                                link.link_name
-                                                  ?.toLowerCase()
-                                                  .includes('linkedin')
-                                              ) && (
-                                                <HiOutlineExternalLink
-                                                  size={20}
-                                                />
-                                              )}
-                                              <p className="font-semibold">
-                                                {link.link_name}
-                                              </p>
-                                            </div>
-                                          </div>
-                                        </a>
-                                      </td>
-                                      <td className="flex items-center gap-x-3 justify-end w-[20%] px-1 py-4">
-                                        <VscEdit
-                                          size={27}
-                                          className="cursor-pointer hover:text-orange-500 font-light"
-                                        />
-                                        <TbTrash
-                                          onClick={() =>
-                                            handleDeleteTableData(link.link_url)
-                                          }
-                                          size={27}
-                                          className="cursor-pointer hover:text-orange-500 font-light"
-                                        />
-                                      </td>
-                                    </div>
-                                  ))}
-                                </tr>
-                              </tbody>
-                            </table>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
+                    <AddedLinksTable
+                      addedLinks={addedLinks}
+                      setAddedLinks={setAddedLinks}
+                      addedDefaultLinks={addedDefaultLinks}
+                      setAddedDefaultLinks={setAddedDefaultLinks}
+                    />
                   </div>
 
                   {/* suggestions  */}
